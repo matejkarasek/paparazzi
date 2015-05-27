b42739590ae4b296a0a5f455319135595a7415c3
matejkarasek <matejkarasek@gmail.com>
2015-05-27 19:33:20 +0200

    Data logging triggered by channel 5

--- sw/airborne/modules/loggers/sd_logger_spi_direct.c
+++ sw/airborne/modules/loggers/sd_logger_spi_direct.c
@@ -66,12 +66,12 @@ void sd_logger_periodic(void)
 {
   /* Check if the switch is flipped to start or stop logging */
   static bool_t sd_logger_previous_switch_state = FALSE;
-  if (USEC_OF_RC_PPM_TICKS(ppm_pulses[5]) > 1300 && sd_logger_previous_switch_state == FALSE) {
+  if (USEC_OF_RC_PPM_TICKS(ppm_pulses[4]) > 1300 && sd_logger_previous_switch_state == FALSE) {
     /* Start logging */
     sdlogger.cmd = SdLoggerCmd_StartLogging;
     sd_logger_command();
     sd_logger_previous_switch_state = TRUE;
-  } else if (USEC_OF_RC_PPM_TICKS(ppm_pulses[5]) < 1300 && sd_logger_previous_switch_state == TRUE) {
+  } else if (USEC_OF_RC_PPM_TICKS(ppm_pulses[4]) < 1300 && sd_logger_previous_switch_state == TRUE) {
     /* Stop logging */
     sdlogger.cmd = SdLoggerCmd_StopLogging;
     sd_logger_command();
