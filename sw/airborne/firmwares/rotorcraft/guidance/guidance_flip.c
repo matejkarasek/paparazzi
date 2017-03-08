4a9ae1903fe9326efcd48a7ff125c92c08bcbf02
matejkarasek <matejkarasek@gmail.com>
2017-03-08 17:59:20 +0100

    Flip updated with new strategy

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_flip.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_flip.c
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef START_RECOVER_CMD_ANGLE
-#define START_RECOVER_CMD_ANGLE -115.0
+#define START_RECOVER_CMD_ANGLE 255.0 //-115.0
 #endif
 
 #ifndef FIRST_THRUST_LEVEL
@@ -83,24 +83,28 @@ void guidance_flip_enter(void)
 void guidance_flip_run(void)
 {
   uint32_t timer;
-  int32_t phi, theta, phiq, thetaq, qi, qx, qy, qz;
+  int32_t phi, theta, phi_gyr, theta_gyr, p, q; //phiq, thetaq, qi, qx, qy, qz;
   static uint32_t timer_save = 0;
-//  struct Int32Quat q, qg, qprod;
+  //  struct Int32Quat q, qg, qprod;
 
   timer = (flip_counter++ << 12) / PERIODIC_FREQUENCY;
   phi = stateGetNedToBodyEulers_i()->phi;
   theta = stateGetNedToBodyEulers_i()->theta;
-//  qi = stateGetNedToBodyQuat_i()->qi;
-//  qx = stateGetNedToBodyQuat_i()->qx;
-//  qy = stateGetNedToBodyQuat_i()->qy;
-  //qz = stateGetNedToBodyQuat_i()->qz;
-//  q = stateGetNedToBodyQuat_i();
 
-// quaternion multiplication
-// int32_quat_comp(struct Int32Quat *a2c, struct Int32Quat *a2b, struct Int32Quat *b2c)
+  p = stateGetBodyRates_i()->p;
+  q = stateGetBodyRates_i()->q;
 
-  phiq = 2*int32_atan2(stateGetNedToBodyQuat_i()->qx, stateGetNedToBodyQuat_i()->qi);
-  thetaq=2*int32_atan2(stateGetNedToBodyQuat_i()->qy, stateGetNedToBodyQuat_i()->qi);
+  //  qi = stateGetNedToBodyQuat_i()->qi;
+  //  qx = stateGetNedToBodyQuat_i()->qx;
+  //  qy = stateGetNedToBodyQuat_i()->qy;
+  //  qz = stateGetNedToBodyQuat_i()->qz;
+  //  q = stateGetNedToBodyQuat_i();
+
+  // quaternion multiplication
+  // int32_quat_comp(struct Int32Quat *a2c, struct Int32Quat *a2b, struct Int32Quat *b2c)
+
+  //  phiq = 2*int32_atan2(stateGetNedToBodyQuat_i()->qx, stateGetNedToBodyQuat_i()->qi);
+  //  thetaq=2*int32_atan2(stateGetNedToBodyQuat_i()->qy, stateGetNedToBodyQuat_i()->qi);
 
 
   switch (flip_state) {
@@ -114,21 +118,29 @@ void guidance_flip_run(void)
       timer_save = 0;
 
       if (timer > BFP_OF_REAL(FIRST_THRUST_DURATION, 12)) {
-        if (FLIP_ROLL && ~FLIP_PITCH) flip_state=1;
-        else if (FLIP_PITCH && ~FLIP_ROLL) flip_state=3;
-        else flip_state=100; // return to attitude mode
+        if (FLIP_ROLL && ~FLIP_PITCH) {
+        	phi_gyr = phi; // initialize the phi estimate with the current phi
+        	flip_state = 1;
+        }
+        else if (FLIP_PITCH && ~FLIP_ROLL) {
+        	theta_gyr = theta; // initialize the theta estimate with the current theta
+        	flip_state = 3;
+        }
+        else flip_state = 100; // return to attitude mode
         // TODO: Add a combined pitch and roll flip
       }
       break;
 
     case 1:
-      stabilization_cmd[COMMAND_ROLL]   = 9000; // Rolling command
+      stabilization_cmd[COMMAND_ROLL]   = 8000; // Rolling command
       stabilization_cmd[COMMAND_PITCH]  = 0;
       stabilization_cmd[COMMAND_YAW]    = 0;
-      stabilization_cmd[COMMAND_THRUST] = 4000; //Min thrust?
+      stabilization_cmd[COMMAND_THRUST] = 5600; // --> Left (5600-8000/2) = 1600, right --> (5600+8000/2) = 9600
 
+      // Integrate gyros for angle estimates
+      phi_gyr += p/PERIODIC_FREQUENCY;   // RATE_FRAC = ANGLE_FRAC
 
-      if (phi > ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
+      if (phi_gyr > ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
         flip_state++;
       }
       break;
@@ -137,37 +149,45 @@ void guidance_flip_run(void)
       stabilization_cmd[COMMAND_ROLL]   = 0;
       stabilization_cmd[COMMAND_PITCH]  = 0;
       stabilization_cmd[COMMAND_YAW]    = 0;
-      stabilization_cmd[COMMAND_THRUST] = 1000; //Min thrust?
+      stabilization_cmd[COMMAND_THRUST] = 1600; // Min. thrust, so that none of the wings stops flapping
 
-      if (phi > ANGLE_BFP_OF_REAL(RadOfDeg(START_RECOVER_CMD_ANGLE)) && phi < ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
+      // Integrate gyros for angle estimates
+      phi_gyr += p/PERIODIC_FREQUENCY;   // RATE_FRAC = ANGLE_FRAC
+
+      if (phi_gyr > ANGLE_BFP_OF_REAL(RadOfDeg(START_RECOVER_CMD_ANGLE))) { // && phi < ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
         timer_save = timer;
         flip_state = 5;
       }
       break;
 
     case 3:
-          stabilization_cmd[COMMAND_ROLL]   = 0; // Rolling command
-          stabilization_cmd[COMMAND_PITCH]  = 9000;
-          stabilization_cmd[COMMAND_YAW]    = 0;
-          stabilization_cmd[COMMAND_THRUST] = 4000; //Min thrust?
+      stabilization_cmd[COMMAND_ROLL]   = 0;
+      stabilization_cmd[COMMAND_PITCH]  = 9600; // Pitching command
+      stabilization_cmd[COMMAND_YAW]    = 0;
+      stabilization_cmd[COMMAND_THRUST] = 9000; // Max. thrust
 
+      // Integrate gyro for pitch estimate
+      theta_gyr += q/PERIODIC_FREQUENCY;   // RATE_FRAC = ANGLE_FRAC
 
-          if (theta > ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
-            flip_state++;
-          }
-          break;
+      if (theta_gyr > ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
+    	  flip_state++;
+      }
+      break;
 
     case 4:
-          stabilization_cmd[COMMAND_ROLL]   = 0;
-          stabilization_cmd[COMMAND_PITCH]  = 0;
-          stabilization_cmd[COMMAND_YAW]    = 0;
-          stabilization_cmd[COMMAND_THRUST] = 1000; //Min thrust?
+      stabilization_cmd[COMMAND_ROLL]   = 0;
+      stabilization_cmd[COMMAND_PITCH]  = 0;
+      stabilization_cmd[COMMAND_YAW]    = 0;
+      stabilization_cmd[COMMAND_THRUST] = 1600; //Min thrust?
 
-          if (theta > ANGLE_BFP_OF_REAL(RadOfDeg(START_RECOVER_CMD_ANGLE)) && theta < ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
-            timer_save = timer;
-            flip_state = 5;
-          }
-          break;
+      // Integrate gyro for pitch estimate
+      theta_gyr += q/PERIODIC_FREQUENCY;   // RATE_FRAC = ANGLE_FRAC
+
+      if (theta_gyr > ANGLE_BFP_OF_REAL(RadOfDeg(START_RECOVER_CMD_ANGLE))) { // && theta < ANGLE_BFP_OF_REAL(RadOfDeg(STOP_FLIP_CMD_ANGLE))) {
+        timer_save = timer;
+        flip_state = 5;
+      }
+      break;
 
     case 5:
       flip_cmd_earth.x = 0;
